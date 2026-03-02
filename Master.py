adda4dd8207b270c8dfb70758c443c05a637f810
Thomas Larsen <Larsen.thomas.dk@gmail.com>
2026-03-02 13:57:04 +0100

    Refactor password cracking logic and enhance communication between master and slave

--- Master.py
+++ Master.py
@@ -6,32 +6,53 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
+crackedPasswords = []
+
+# Get the hashed passwords
 with open("passwords.txt", "r") as file:
     passwordList = file.read().splitlines()
 
+# Get the dictionary words
 with open("webster-dictionary.txt", "r") as file:
     WordList = file.read().splitlines()
 
-chunkSize = 10000
+chunkSize = 1000 # Number of words in each chunk
+
+# Split the word list into chunks
 WordList =[WordList[i:i + chunkSize] for i in range(0, len(WordList), chunkSize)]
 
-currentChunk = 0
+
+chuckAmount = len(WordList) # Number of chunks to distribute among clients
+currentChunk = 0 # Index of the current chunk being distributed to clients
 
 def handleClient(connectionSocket):
+    global currentChunk
+    print("Client connected: " + str(connectionSocket.getpeername()))
     while True:
         message = connectionSocket.recv(1024).decode()
-        
         match message:
             case "ready":
                 continue
             case "chunk":
-                sentence = WordList[currentChunk]
-                currentChunk = (currentChunk + 1) % len(WordList)
+                currentChunk = (currentChunk + 1) % len(WordList) # Gets next chunk index
+                connectionSocket.send(str(WordList[currentChunk]).encode())
             case "password":
-                sentence = passwordList
+                connectionSocket.send(str(passwordList).encode())
+            case "done":
+                print("Client has finished cracking passwords.")
+                break
             case _:
+                # Check if the message indicates a found password
+                if message.startswith("found:"):
+                    password = message.split("found:")[1]
+                    print("Client found password: " + password)
+                    crackedPasswords.append(password)
+                
+                # If all chunks have been distributed, print the cracked passwords
+                if currentChunk == chuckAmount - 1:
+                    print(crackedPasswords)
                 continue
-        connectionSocket.send(sentence.upper().encode())
+
 
 while True:
     connectionSocket, addr = serverSocket.accept()
--- Slave.py
+++ Slave.py
@@ -1,4 +1,7 @@
+import hashlib
+import base64
 from socket import *
+import ast
 
 # Client setup
 serverName = 'localhost'
@@ -7,58 +10,113 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
 chunk = []
-passwordList = []
+passwordDictionary = {}
 
 
-def crackPassword(chunk, passwordList):
+def crackPassword(password):
+    global chunk
     for word in chunk:
-        word = word.lower()
-        word.hash = hash(word)
-        for password in passwordList:
-            if(password == word):
-                print("Password found: " + password)
-                return password
+        print("Trying password: " + word)
+        lowerWord = word.lower()
+        hashedWord = get_sha1_base64(lowerWord)
+        if(password == hashedWord):
+            print("Password found: " + lowerWord)
+            return lowerWord
             
-        word = word.upper()
-        word.hash = hash(word)
-        for password in passwordList:
-            if(password == word):
-                print("Password found: " + password)
-                return password
+        upperWord = word.upper()
+        hashedWord = get_sha1_base64(upperWord)
+        if(password == hashedWord):
+            print("Password found: " + upperWord)
+            return upperWord
             
-        word = word.capitalize()
-        word.hash = hash(word)
-        for password in passwordList:
-            if(password == word):
-                print("Password found: " + password)
-                return password
+        capitalizedWord = word.capitalize()
+        hashedWord = get_sha1_base64(capitalizedWord)
+        if(password == hashedWord):
+            print("Password found: " + capitalizedWord)
+            return capitalizedWord
             
-        word = reversed(word)
-        word.hash = hash(word)
-        for password in passwordList:
-            if(password == word):
-                print("Password found: " + password)
-                return password
+        reverseWord = reverse(word)
+        hashedWord = get_sha1_base64(reverseWord)
+        if(password == hashedWord):
+            print("Password found: " + reverseWord)
+            return reverseWord
+        
+        for i in range(100):
+            str(i) + word
+            hashedWord = get_sha1_base64(str(i) + word) 
+            if(password == hashedWord):
+                print("Password found: " + str(i) + word)
+                return str(i) + word
+        
+        for i in range(100):
+            numberword = word + str(i)
+            hashedWord = get_sha1_base64(numberword) 
+            if(password == hashedWord):
+                print("Password found: " + numberword)
+                return numberword
+            
+        for i in range(10):
+            for j in range(10):
+                str(j) + word + str(i)
+                hashedWord = get_sha1_base64(str(j) + word + str(i)) 
+                if(password == hashedWord):
+                    print("Password found: " + str(j) + word + str(i))
+                    return str(j) + word + str(i)
+    return None
 
+def get_sha1_base64(input_string):
+    # 1. Convert string to bytes
+    data = input_string.encode('utf-8')
+    
+    # 2. Get the SHA-1 hash (raw bytes, not hex)
+    sha1_bytes = hashlib.sha1(data).digest()
+    
+    # 3. Encode those bytes to Base64
+    base64_hash = base64.b64encode(sha1_bytes).decode('utf-8')
+    
+    return base64_hash
 
 def reverse(sentence):
     return sentence[::-1]
 
+first = True
+
+
 # input and send data
 while True:
-    sentence = "ready"
-    clientSocket.send(sentence.encode())
     # receive and print modified data
     if(chunk == []):
+        print("Requesting chunk from server...")
         clientSocket.send("chunk".encode())
-        chunk = clientSocket.recv(1024).decode()
+        print("Receiving chunk from server...")
+        recievedChunk = clientSocket.recv(100000).decode()
+        try:
+            chunk = ast.literal_eval(recievedChunk)
+        except Exception as e:
+            print(f"Error parsing chunk: {e}")
+            continue
         continue
     
-    elif(passwordList == []):
+    elif(passwordDictionary == {}):
+        print("Requesting password list from server...")
         clientSocket.send("password".encode())
-        passwordList = clientSocket.recv(1024).decode()
+        print("Receiving password list from server...")
+        passwordList = clientSocket.recv(100000).decode()
+        print("Password list received from server: " + passwordList)
+        
+        passwordList = ast.literal_eval(passwordList)
+        print("Password list converted to list: " + str(passwordList))
+        for i in range(len(passwordList)):
+            passwordDictionary[passwordList[i].split(":")[0]] = passwordList[i].split(":")[1]
+            passwordList[i] = passwordList[i].split(":")[1]
         continue
         
-    if(chunk != [] and passwordList != []):
-        password = crackPassword(chunk, passwordList)
-        clientSocket.send(password.encode())
+    if(chunk != [] and passwordDictionary != {}):
+        print("Cracking password...")
+        for username in passwordDictionary:
+            crackedpassword = crackPassword(passwordDictionary[username])
+            if crackedpassword:
+                print("Password cracked for user " + username + ": " + crackedpassword)
+                clientSocket.send(("found:" + username + ":" + crackedpassword).encode())
+        clientSocket.send("done".encode())
+        break
